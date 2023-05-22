885f0a6fd299d6188d7102c5c98f75878578a62f
Xumpocmb <x.xumpocmb@gmail.com>
2023-05-22 14:28:24 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Audio From Video
+
+## A simple script for extract audio from video
---
+++ main.py
@@ -0,0 +1,22 @@
+import moviepy.editor
+from pathlib import Path
+import os
+
+
+folder = input('Введите путь к видео: ')
+file_name = input('Введите имя файла с расширением: ')
+full_path = os.path.join(folder, file_name)
+if Path(full_path).is_file():
+    print('[+] Файл найден. Начинаю работу!')
+    video = moviepy.editor.VideoFileClip(full_path)
+    audio = video.audio
+    try:
+        # работа с файлом, stem - взять только имя файла, без расширения
+        # audio.write_audiofile(f'{os.path.splitext(file_name)[0]}_audio.mp3')
+        audio.write_audiofile(f'{Path(full_path).stem}.mp3')
+    except Exception as ex:
+        print(f'Ошибка: {ex}')
+
+
+
+
